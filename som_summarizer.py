c491bb044242f8c8ea2a9c35419b57ac36103278
Joe Zuntz <joezuntz@googlemail.com>
2021-07-28 12:31:40 +0100

    add stream mode and save images

--- som_summarizer.py
+++ som_summarizer.py
@@ -3,6 +3,7 @@ import xpysom
 import h5py
 import pickle
 import yaml
+import sys
 # 1) make and save the deep som
 # 2) make and save the wide som
 # 3) use the redshift and overlap samples to complete the process
@@ -26,14 +27,13 @@ class LupticolorSom:
         self.nband = len(self.bands)
         self.nfeat = self.nband - 1 if self.use_band is None else self.nband
 
-
         self.comm = comm
 
         # Make our SOM
         self.som = xpysom.XPySom(self.dim, self.dim, self.nfeat, sigma=self.sigma, learning_rate=self.rate, topology='toroidal')
 
 
-    def add_data(self, indata):
+    def train(self, indata):
         names, data = self.make_normalized_color_data(indata)
         self.som.train(data, self.epochs, comm=self.comm)
 
@@ -97,17 +97,20 @@ class SOMSummarizer:
     def __init__(self, config, comm=None):
         self.config = config
         self.comm = comm
+        self.rank = 0 if comm is None else self.comm.rank
+        self.nproc = 1 if comm is None else self.comm.size
 
     def run(self):
         deep_som = self.make_som(self.config['deep'])
         self.save_som(deep_som, self.config['output']['deep_som'])
 
         # Could save this out here also
-        deep_zmap = self.make_deep_zmap(deep_som)
-        # wide_som = self.make_som(self.config['wide'])
+        deep_zmaps = self.make_deep_zmaps(deep_som)
+        self.save_maps(deep_zmaps, self.config['output']['deep_name'])
+
 
     def save_som(self, som, outfile):
-        if self.comm is None or self.comm.rank == 0:
+        if self.rank == 0:
             pickle.dump(som, open(outfile, 'wb'))
 
 
@@ -118,11 +121,11 @@ class SOMSummarizer:
         fmt = config.get('band_name', 'mag_{}')
         bands = config['bands']
         nmax = config.get('nmax')
+        random_order = config.get('random_chunk_order', True)
+        order_seed = config.get('order_seed', 1234567)
         extra = extra or {}
 
         cols = [fmt.format(b) for b in bands]
-        rank = 0 if self.comm is None else self.comm.rank
-        size = 1 if self.comm is None else self.comm.size
 
 
         with h5py.File(filename) as f:
@@ -132,15 +135,44 @@ class SOMSummarizer:
                 sz = min(nmax, sz)
 
             if stream:
-                # should randomize the ordering here I think, just
-                # in case there is structure in the data
-                raise NotImplementedError("Sorry, not implemented")
-                # for loop over data
+                max_chunk_size = config.get('max_chunk_size', 100_000)
+                # we need a number of chunks that splits the entire
+                # data set into chunks which:
+                # i) are a multiple of the number of MPI processes
+                # ii) are smaller than max_chunk_size
+                nchunk = int(np.ceil(sz / max_chunk_size / self.nproc)) * self.nproc
+                chunk_size = int(np.ceil(sz / nchunk))
+                chunk_starts = np.arange(nchunk) * chunk_size
+                chunk_ends = chunk_starts + chunk_size
+                chunk_order = np.arange(len(chunk_starts))
+
+                # We (optionally) randomize the order of the chunk.
+                # This is because if the input is e.g. ordered by redshift
+                # then that could bias the training.
+                if random_order:
+                    rng = np.random.default_rng(seed=order_seed)
+                    rng.shuffle(chunk_order)
+                    if self.rank == 0:
+                        print(f"Will load {nchunk} data chunks in order: {chunk_order}")
+                    if self.comm:
+                        self.comm.Bcast(chunk_order)
+
+                for i, chunk_index in enumerate(chunk_order):
+                    if i % self.nproc != self.rank:
+                        continue
+                    s = chunk_starts[chunk_index]
+                    e = chunk_ends[chunk_index]
+                    print(f"Rank {self.rank} reading {filename} data range {s} - {e} (chunk {i+1} / {nchunk})")
+                    data = {b: g[col][s:e] for b,col in zip(bands,cols)}
+                    for name, col in extra.items():
+                        data[name] = g[col][s:e]
+                    yield data
+
             else:
-                block_size = int(np.ceil(sz / size))
-                s = block_size * rank
+                block_size = int(np.ceil(sz / self.nproc))
+                s = block_size * self.rank
                 e = s + block_size
-                print(f"Rank {rank} reading {filename} data range {s} - {e} from total range 0 - {sz}")
+                print(f"Rank {self.rank} reading {filename} its full data range {s} - {e}")
                 data = {b: g[col][s:e] for b,col in zip(bands,cols)}
                 for name, col in extra.items():
                     data[name] = g[col][s:e]
@@ -159,11 +191,11 @@ class SOMSummarizer:
         data_stream = self.data_stream(data_config)
 
         for data in data_stream:
-            som.add_data(data)
+            som.train(data)
 
         return som
 
-    def make_deep_zmap(self, som):
+    def make_deep_zmaps(self, som):
 
         zsum = np.zeros((som.dim, som.dim))
         zsum2 = np.zeros((som.dim, som.dim))
@@ -182,15 +214,46 @@ class SOMSummarizer:
                 count[wi] += 1
         zmean = zsum / count
         zsigma = np.sqrt(zsum2 / count - zmean**2) 
-        outfile = self.config["output"]["zmaps"]
-        pickle.dump((count, zmean, zsigma), open(outfile, "wb"))
+
 
         return count, zmean, zsigma
+
+    def save_maps(self, maps, name):
+        if self.rank != 0:
+            return
+
+        count, zmean, zsigma = maps
+
+        pickle.dump(count, open(f'{name}_zcount.pkl', 'wb'))
+        pickle.dump(zmean, open(f'{name}_zmean.pkl', 'wb'))
+        pickle.dump(zsigma, open(f'{name}_zsigma.pkl', 'wb'))
+
+        import matplotlib.pyplot as plt
+        from matplotlib.colors import LogNorm
+        fig, axes = plt.subplots(3, 1, figsize=(4, 10))
+        im = axes[0].imshow(count, norm=LogNorm(vmin=1, vmax=count.max()))
+        plt.colorbar(im, ax=axes[0])
+        axes[0].set_title("count")
+
+        im = axes[1].imshow(zmean)
+        plt.colorbar(im, ax=axes[1])
+        axes[1].set_title("mean")
+
+        img = axes[2].imshow(zsigma)
+        plt.colorbar(im, ax=axes[2])
+        axes[2].set_title("std dev")
+
+        fig.savefig(f"{name}.png")
+
+
 
 
 def main():
     config = yaml.safe_load(open("config.yml"))
-    ss = SOMSummarizer(config)
+    from mpi4py.MPI import COMM_WORLD as world
+    comm = None if world.size == 1 else world
+
+    ss = SOMSummarizer(config, comm)
     ss.run()
 
 if __name__ == '__main__':
